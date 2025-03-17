28dc3cf3926e44e2153bc37410dbfef12d7b0ab9
Kiwee <tjliqy@163.com>
2025-03-17 03:18:43 +0000

    feat: 2.7.3 data

--- node/generate-pages/generate-pages-page-generator-config.js
+++ node/generate-pages/generate-pages-page-generator-config.js
@@ -131,10 +131,10 @@ class _PageGeneratorCrcalculator extends PageGeneratorGeneric {
 class _PageGeneratorIndex extends PageGeneratorGeneric {
 	_filename = "page/template-page-index.hbs";
 	_page = "index.html";
-	_pageDescription = "一套为了《龙与地下城5E》玩家和地下城主而存在的工具网站。";
+	_pageDescription = "一套为了《龙与地下城 DND 5E》玩家和地下城主而存在的工具网站。";
 
 	_navbarTitleHtml = `5e<span>tools</span>`;
-	_navbarDescription = "一套为了《龙与地下城5E》玩家和地下城主而存在的工具网站。";
+	_navbarDescription = "一套为了《龙与地下城 DND 5E》玩家和地下城主而存在的工具网站。";
 	_navbarPageTitleStyleAdditional = "page__title--home";
 
 	_isFontAwesome = true;
