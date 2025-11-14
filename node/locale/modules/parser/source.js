12b52933fc71bc9d9a2bdf9bdfabd296a49d8bb3
Kiwee <tjliqy@163.com>
2025-11-14 04:25:04 +0000

    docs: MCP自动校对DMG和SCAG

--- node/locale/modules/parser/source.js
+++ node/locale/modules/parser/source.js
@@ -3,8 +3,8 @@ import { transferLangConfig } from "../../config.js";
 const config = {
 	"drde": ["巨龙迷城", "巨龙迷城", "Dragon Deleves"],
 	"screen": ["地下城主帷幕", "地下城主帷幕", "Dungeon Master's Screen"],
-	"screenwildernesskit": ["地下城主帷幕：荒野", "", "Dungeon Master's Screen: Wilderness Kit"],
-	"screendungeonkit": ["地下城主帷幕：龙", "", "Dungeon Master's Screen: Dungeon Kit"],
+	"screenwildernesskit": ["地下城主帷幕：荒野套组", "", "Dungeon Master's Screen: Wilderness Kit"],
+	"screendungeonkit": ["地下城主帷幕：地下城套组", "", "Dungeon Master's Screen: Dungeon Kit"],
 	"screenspelljammer": ["地下城主帷幕：魔法船", "", "Dungeon Master's Screen: Spelljammer"],
 	"xscreen": ["地下城主帷幕（2024）", "", "Dungeon Master's Screen (2024)"],
 	"dmtcrg": ["万象无常牌：参考卡", "", "The Deck of Many Things: Card Reference Guide"],
