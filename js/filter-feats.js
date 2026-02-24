fae6cf8a61394ed3255cf394082d7934dcea57c0
Kiwee <tjliqy@163.com>
2026-02-24 12:12:16 +0000

    feat: 修复标识符前后颠倒的问题

--- js/filter-feats.js
+++ js/filter-feats.js
@@ -254,7 +254,6 @@ class ModalFilterFeats extends ModalFilterBase {
 				ability: feat._slAbility,
 				prerequisite: feat._slPrereq,
 				ENG_name: feat.ENG_name,
-				ENG_hash: UrlUtil.autoEncodeEngHash(feat),
 			},
 			{
 				cbSel: eleRow.firstElementChild.firstElementChild.firstElementChild,
