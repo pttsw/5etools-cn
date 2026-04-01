2e49b8ea6ea0c311859d00569160b1127163ed50
Kiwee <tjliqy@163.com>
2026-04-01 02:12:15 +0000

    fix: 修复第三方导入后物品页不显示的问题

--- js/parser.js
+++ js/parser.js
@@ -2549,7 +2549,7 @@ Parser.RARITIES_TO_CN = {
 };
 
 Parser.rarityToCN = function (rarity) {
-	return Parser._parse_aToB(Parser.RARITIES_TO_CN, rarity.toLowerCase()) || rarity;
+	return Parser._parse_aToB(Parser.RARITIES_TO_CN, (rarity || "").toLowerCase()) || rarity;
 };
 Parser.CAT_ID_CREATURE = 1;
 Parser.CAT_ID_SPELL = 2;
